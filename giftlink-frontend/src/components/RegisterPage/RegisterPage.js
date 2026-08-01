c0d41c08e522d76f11aef4770000b8ee47298c2c
AminAgrebi-lab <amin.agrebi2026@outlook.com>
2026-08-01 05:38:39 -0400

    Implement handleRegister logic and update Navbar component

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -1,6 +1,15 @@
 import React, { useState } from 'react';
 import './RegisterPage.css';
 
+// Task 1: Import urlConfig from `giftlink-frontend/src/config.js`
+import { urlConfig } from '../../config';
+
+// Task 2: Import useAppContext `giftlink-frontend/context/AuthContext.js`
+import { useAppContext } from '../../context/AuthContext';
+
+// Task 3: Import useNavigate from `react-router-dom` to handle navigation after successful registration.
+import { useNavigate } from 'react-router-dom';
+
 function RegisterPage() {
 
     // useState hook variables for firstName, lastName, email, password
@@ -9,9 +18,57 @@ function RegisterPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    // Task 4: Include a state for error message.
+    const [showerr, setShowerr] = useState('');
+
+    // Task 5: Create a local variable for `navigate` and `setIsLoggedIn`.
+    const navigate = useNavigate();
+    const { setIsLoggedIn } = useAppContext();
+
     // handleRegister function
     const handleRegister = async () => {
-        console.log("Register invoked");
+        try {
+            const response = await fetch(`${urlConfig.backendUrl}/api/auth/register`, {
+                // Task 6: Set method
+                method: 'POST',
+
+                // Task 7: Set headers
+                headers: {
+                    'content-type': 'application/json',
+                },
+
+                // Task 8: Set body to send user details
+                body: JSON.stringify({
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email,
+                    password: password,
+                }),
+            });
+
+            // Step 2 - Task 1: Access data coming from fetch API
+            const json = await response.json();
+
+            // Step 2 - Task 2: Set user details
+            if (json.authtoken) {
+                sessionStorage.setItem('auth-token', json.authtoken);
+                sessionStorage.setItem('name', firstName);
+                sessionStorage.setItem('email', json.email);
+
+                // Step 2 - Task 3: Set the state of user to logged in using the useAppContext
+                setIsLoggedIn(true);
+
+                // Step 2 - Task 4: Navigate to the MainPage after logging in
+                navigate('/app');
+            } else {
+                // Step 2 - Task 5: Set an error message if the registration fails
+                if (json.error) {
+                    setShowerr(json.error);
+                }
+            }
+        } catch (e) {
+            console.log("Error fetching details: " + e.message);
+        }
     };
 
     return (
@@ -20,6 +77,9 @@ function RegisterPage() {
                 <div className="col-md-6 col-lg-4">
                     <div className="register-card p-4 border rounded">
                         <h2 className="text-center mb-4 font-weight-bold">Register</h2>
+
+                        {/* Step 2 - Task 6: Display error message to end user */}
+                        {showerr && <div className="text-danger mb-3">{showerr}</div>}
 
                         {/* First Name Input */}
                         <div className="mb-3">
